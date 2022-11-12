9344e03ef1263316b3784b2840c6ebb89313d661
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-12 07:41:44 -0700

    Navigation & Start of Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Project from "./components/Project";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function About() {
+export default function Resume() {
     return (
         <div>
-            <h1>About Me</h1>
+            <h1>Resume</h1>
             <p>
 
             </p>
