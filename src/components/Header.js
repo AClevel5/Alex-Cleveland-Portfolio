6b5eaf2ca089374c22921216c1ad3f7ec49149bf
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-14 17:20:55 -0700

    Mountain Banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../styles/Header.css';
-import NavTabs from './Navigation';
+// import NavTabs from './Navigation';
+
 
 
 function Header() {
 
     return (
+
         <header className="header">
             <h1>Alex Cleveland</h1>
-            <NavTabs />
         </header>
+
     );
 }
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import NavTabs from "./Navigation"
 
 function Project() {
     return (
         <div>
 
             <Header />
+            <NavTabs />
             <Footer />
 
         </div>
