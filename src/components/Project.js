82a9943911b631430f7bfb4d1211112de766fe0a
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-14 18:25:13 -0700

    Stopping point page change is broken

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,32 @@
 import React from "react";
-import About from "./pages/About"
+import { useState } from "react";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+
 
 function Project() {
-    return (
-        <div>
-            <About />
+    const [currentPage, setCurrentPage] = useState("About");
 
+    const renderPage = () => {
+        if (currentPage === "About") {
+            return <About />;
+        }
+        if (currentPage === "Contact") {
+            return <Contact />;
+        }
+        if (currentPage === "Portfolio") {
+            return <Portfolio />;
+        }
+        return <Resume />;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div currentPage={currentPage} handlePageChange={handlePageChange}>
+            {renderPage()}
         </div>
     );
 }
