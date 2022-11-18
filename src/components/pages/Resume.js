04ec98fcc175ee9781789a149a14f230396f4667
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-17 17:02:27 -0700

    Resume page tweaks

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,31 +5,40 @@ import "../../styles/Resume.css";
 export default function Resume() {
     return (
         <div>
-            <h1>Resume</h1>
-            <h4>View my <a href={resume} target="_blank" rel="noreferrer" class="resume">Resume</a></h4>
-            <h2>Front-end Proficiencies:</h2>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>ReactJS</li>
-                <li>Bootstrap</li>
-                <li>Bulma</li>
-                <li>Progressive Web Apps</li>
-            </ul>
-            <h2>Back-end Proficiencies:</h2>
-            <ul>
-                <li>SQL</li>
-                <li>MySQL, Sequelize</li>
-                <li>MongoDB</li>
-                <li>Mongoose</li>
-                <li>MERN</li>
-                <li>REST</li>
-                <li>Node.js</li>
-                <li>Express</li>
-                <li>GraphQL</li>
-            </ul>
-
+            <div class="head">
+                <h1>Resume</h1>
+                <h4>View my <a href={resume} target="_blank" rel="noreferrer" class="resume">Resume</a></h4>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h2>Front-end Proficiencies:</h2>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>ReactJS</li>
+                            <li>Bootstrap</li>
+                            <li>Bulma</li>
+                            <li>Progressive Web Apps</li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <h2>Back-end Proficiencies:</h2>
+                        <ul>
+                            <li>SQL</li>
+                            <li>MySQL, Sequelize</li>
+                            <li>MongoDB</li>
+                            <li>Mongoose</li>
+                            <li>MERN</li>
+                            <li>REST</li>
+                            <li>Node.js</li>
+                            <li>Express</li>
+                            <li>GraphQL</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
