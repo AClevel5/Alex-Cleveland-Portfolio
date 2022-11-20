3023b2aed23f551b9310ec3bedf080b4f5127976
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-20 10:23:56 -0700

    Project data start

---
+++ src/components/ProjectData.js
@@ -0,0 +1,63 @@
+
+
+
+
+export const projects = [
+
+    {
+        name: "Powder Bound",
+        url: "https://aclevel5.github.io/ski-planner/",
+        gitHubUrl: "https://github.com/AClevel5/ski-planner",
+        image: "",
+
+
+    },
+    {
+        name: "HiKamp",
+        url: "https://hikamp.herokuapp.com/",
+        gitHubUrl: "https://github.com/AClevel5/hiKamp",
+        image: "",
+
+
+    },
+    {
+        name: "Text Editor",
+        url: "https://text-editor-alexc.herokuapp.com/",
+        gitHubUrl: "https://github.com/AClevel5/Text-Editor",
+        image: "",
+
+
+    },
+    {
+        name: "Note Taker",
+        url: "https://arc5-note-taker.herokuapp.com/",
+        gitHubUrl: "https://github.com/AClevel5/Note-Taker",
+        image: "",
+
+
+    },
+    {
+        name: "Team Profile Generator",
+        url: "https://github.com/AClevel5/Team-Profile-Generator",
+        gitHubUrl: "https://github.com/AClevel5/Team-Profile-Generator",
+        image: "",
+
+
+    },
+    {
+        name: "Weather App",
+        url: "https://alex-tech-blog7.herokuapp.com/",
+        gitHubUrl: "https://github.com/AClevel5/Tech-Blog",
+        image: "",
+
+
+    }
+
+
+
+
+
+
+
+
+]
